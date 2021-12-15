b435a61fd4928f2c1135ae18c91f6216d397c467
f.parmoo@gmail.com <parmoo6008>
2021-12-15 11:05:38 +0330

    Editing Selenium Implicit/Explicit waiting

--- seleniumexplicitwait.py
+++ seleniumexplicitwait.py
@@ -2,10 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get('http://www.webscrapingfordatascience.com/complexjavascript/')
-explicit_wait_driver = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'quote')))
 
-for item in explicit_wait_driver:
+quote_count = 0
+while True:
+    chain = ActionChains(driver)
+    chain.move_to_element(driver.find_element(By.CLASS_NAME, 'infinite-scroll'))
+    chain.click()
+    chain.send_keys([Keys.PAGE_DOWN for _ in range(100)])
+    chain.perform()
+
+    result = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'quote')))
+    if len(result) == quote_count:
+        break
+    else:
+        quote_count = len(result)
+
+print('Line Count:', len(result))
+for item in result:
     print(item.text)
+
+input('Please presss any ket to exit...')
+driver.quit()
--- seleniumimplicitwait.py
+++ seleniumimplicitwait.py
@@ -1,13 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-print('Implicit Wait...')
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 url = 'http://www.webscrapingfordatascience.com/complexjavascript/'
 driver.get(url)
-for item in driver.find_elements(By.CLASS_NAME, "quote"):
+
+line_count = 0
+while True:
+    chain = ActionChains(driver)
+    chain.move_to_element(driver.find_element(By.CLASS_NAME, 'infinite-scroll'))
+    chain.click()
+    chain.send_keys([Keys.PAGE_DOWN for _ in range(100)])
+    chain.perform()
+    driver.implicitly_wait(10)
+    result = driver.find_elements(By.CLASS_NAME, "quote")
+    if line_count == len(result):
+        break
+    else:
+        line_count = len(result)
+
+print('Line Count:', len(result))
+for item in result:
     print(item.text)
+
 input('Please Enter a Key to Stop...')
 driver.quit()
-
-
